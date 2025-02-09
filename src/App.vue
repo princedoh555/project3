3b52a72e188c53c8bb6fbd22a5f73a3887023ffe
Lawrence Doh <princedoh17@gmail.com>
2025-02-09 08:57:24 -0600

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const name = ref('')
+
+const greeting = computed(() => {
+  const hour = new Date().getHours()
+  if (hour < 12) return 'Good morning'
+  if (hour < 18) return 'Good afternoon'
+  return 'Good evening'
+})
+</script>
+
+<template>
+
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        {{ greeting }}, 
+        <input type="text" placeholder="Enter name" v-model="name">
+      </h2>
+      <p v-if="name">good to see you, {{ name }}!</p>
+
+    </section>
+
+</main>
+
+</template>
+
+
